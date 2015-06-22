f43ae9be5e8f6f6a0223e99524707eb36bcb5995
Jamie Scott <jamie@atomicjets.com>
2015-06-22 12:02:14 +0100

    ad startcoin branding

--- src/js/services/backupService.js
+++ src/js/services/backupService.js
@@ -76,7 +76,7 @@ angular.module('copayApp.services')
       if (!ew) return cb('Could not create backup');
 
       var walletName = (fc.alias || '') + (fc.alias ? '-' : '') + fc.credentials.walletName;
-      var filename = walletName + '-Copaybackup.aes.json';
+      var filename = walletName + '-StartWalletbackup.aes.json';
       _download(ew, filename, cb)
     };
     return root;
